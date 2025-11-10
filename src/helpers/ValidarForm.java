f29674f7194186d2572c24bed9934711d9371fe2
Carlos Eduardo Dias Prado <89024002+Cadf251@users.noreply.github.com>
2025-11-10 19:54:40 -0300

    Add files via upload

---
+++ src/helpers/ValidarForm.java
@@ -0,0 +1,39 @@
+package helpers;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class ValidarForm {
+    /**
+     * Verifica se todos os campos de um form estão preenchidos
+     * 
+     * @param campos
+     * @return boolean true Se estiverem todos preenchidos, false se houber algum campo vazio
+     */
+    public static boolean camposPreenchidos(String[] campos){
+        for(int i=0; i<campos.length;i++){
+            if(campos[i].equals("")){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    /**
+     * Transforma um String recebido de um campo formatado em Date no formato yyyy-MM-dd (Formato do banco de dados)
+     * 
+     * @param data A data em String recebida no formato dd/mm/yyyy
+     * @return Date O object do Date
+     * @throws ParseException Lança a ParseException se ocorrer algum erro.
+     */
+    public static String formatarData(String dataStr) throws ParseException{
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        // Compara a data atual com a data passada
+        Date data = new Date(sdf.parse(dataStr).getTime());
+        System.out.println(data);
+        return "";
+    }
+    
+}
---
+++ src/models/ConexaoDataBase.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelos;
+
+/**
+ * Esta classe serve para iniciar uma nova conexão com o banco de dados SQL usando as credenciais do projeto.
+ * 
+ * Ela é ABSTRACT porque será herdada pelas demais classes do pacote modelos.
+ * @author cadud
+ */
+public abstract class ConexaoDataBase {
+    
+    
+}
